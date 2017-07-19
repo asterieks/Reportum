543970fdcb8eb8dbbeaf10416e6e1185b93694c3
asterieks <asterieks@gmail.com>
2017-07-19 20:46:53 +0200

    war for deployment 3

--- src/main/java/com/reportum/angular2/springmvc/configuration/DataBaseOracleConfig.java
+++ src/main/java/com/reportum/angular2/springmvc/configuration/DataBaseOracleConfig.java
@@ -36,7 +36,7 @@ public class DataBaseOracleConfig {
     public DriverManagerDataSource dataSource() {
         DriverManagerDataSource driverManagerDataSource = new DriverManagerDataSource();
         driverManagerDataSource.setDriverClassName("oracle.jdbc.driver.OracleDriver");
-        driverManagerDataSource.setUrl("jdbc:oracle:thin:@172.26.156.105:1521:XE");
+        driverManagerDataSource.setUrl("jdbc:oracle:thin:@172.26.156.105:1521:ORA11G");
         //driverManagerDataSource.setUrl("jdbc:oracle:thin:@localhost:1521:XE");
         driverManagerDataSource.setUsername("REPORT");
         driverManagerDataSource.setPassword("REPORTUM");
