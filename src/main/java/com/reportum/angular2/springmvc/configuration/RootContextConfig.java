11800d110d347568b43b2e33ae2327a5234e983d
asterieks <asterieks@gmail.com>
2018-01-09 00:16:13 +0100

    deployment

--- src/main/java/com/reportum/angular2/springmvc/configuration/RootContextConfig.java
+++ src/main/java/com/reportum/angular2/springmvc/configuration/RootContextConfig.java
@@ -2,14 +2,9 @@ package com.reportum.angular2.springmvc.configuration;
 
 import com.reportum.angular2.springmvc.configuration.dbconfig.AppDataSourceConfig;
 import com.reportum.angular2.springmvc.configuration.security.SecurityConfiguration;
-import com.reportum.angular2.springmvc.service.IJobService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
-import org.springframework.context.annotation.Profile;
-import org.springframework.context.event.ContextStartedEvent;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.stereotype.Controller;
 
@@ -21,14 +16,4 @@ import static org.springframework.context.annotation.FilterType.ANNOTATION;
 @ComponentScan(basePackages = "com.reportum.angular2.springmvc",
                excludeFilters = {@ComponentScan.Filter(type = ANNOTATION, value = Configuration.class),
                                  @ComponentScan.Filter(type = ANNOTATION, value = Controller.class)  })
-public class RootContextConfig implements ApplicationListener<ContextStartedEvent > {
-
-    @Autowired
-    private IJobService jobService;
-
-    @Override
-    @Profile("dev")
-    public void onApplicationEvent(ContextStartedEvent event) {
-        jobService.checkStateAndUpdate();
-    }
-}
+public class RootContextConfig {}
