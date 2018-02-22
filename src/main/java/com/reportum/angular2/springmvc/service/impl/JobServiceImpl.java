09c193a9b386133ae48682d5bff773f148eaef67
asterieks <asterieks@gmail.com>
2018-02-22 23:09:08 +0100

    fixed issue with app properties

--- src/main/java/com/reportum/angular2/springmvc/service/impl/JobServiceImpl.java
+++ src/main/java/com/reportum/angular2/springmvc/service/impl/JobServiceImpl.java
@@ -2,16 +2,15 @@ package com.reportum.angular2.springmvc.service.impl;
 
 import com.reportum.angular2.springmvc.dao.IJobStateDAO;
 import com.reportum.angular2.springmvc.persistence.entities.JobStateHolder;
+import com.reportum.angular2.springmvc.persistence.entities.Project;
 import com.reportum.angular2.springmvc.service.IEmailService;
 import com.reportum.angular2.springmvc.service.IJobService;
-import com.reportum.angular2.springmvc.persistence.entities.Project;
 import com.reportum.angular2.springmvc.service.IProjectService;
 import com.reportum.angular2.springmvc.utils.enums.State;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
@@ -21,7 +20,6 @@ import java.util.stream.Collectors;
 import static org.apache.commons.collections.CollectionUtils.isEmpty;
 
 @Service
-@PropertySource("classpath:application.properties")
 public class JobServiceImpl implements IJobService {
 
     private static final Long JOB_ID = 1L;
@@ -52,6 +50,10 @@ public class JobServiceImpl implements IJobService {
     @Scheduled(cron = "${reset.state.time}")
     public void updateProjectsStates(){
         DEBUG_LOG.info("JobServiceImpl.updateProjectsStates(): Update state job started.");
+        DEBUG_LOG.debug("adminEmail: "+ adminEmail);
+        DEBUG_LOG.debug("subject: "+ subject);
+        DEBUG_LOG.debug("updateMessage: "+ updateMessage);
+        DEBUG_LOG.debug("remindMessage: "+ remindMessage);
 
         List<Project> projects=projectService.findAllProjects();
         if(!isEmpty(projects)){
@@ -61,7 +63,12 @@ public class JobServiceImpl implements IJobService {
             jobStateHolder.setStateDate(new Date());
             updateJobStateHolder(jobStateHolder);
             DEBUG_LOG.info("JobServiceImpl.updateProjectsStates(): Job completed.");
-            emailService.sendSimpleMessage(adminEmail,subject,updateMessage);
+            try {
+                emailService.sendSimpleMessage(adminEmail,subject,updateMessage);
+            } catch (Exception e){
+                DEBUG_LOG.debug("JobServiceImpl.updateProjectsStates(): failed while mail sending" + e.toString());
+                e.printStackTrace();
+            }
         }
     }
 
@@ -85,7 +92,12 @@ public class JobServiceImpl implements IJobService {
             DEBUG_LOG.debug("JobServiceImpl.checkAndRemind(): An notification has to be sent.");
             Map<String,String> receivers = mapProjectsToReceivers(delayedProjects);
             receivers.forEach((email,projects)->{
-                emailService.sendSimpleMessage(email,subject,buildMessage(projects));
+                try {
+                    emailService.sendSimpleMessage(email,subject,buildMessage(projects));
+                } catch (Exception e){
+                    DEBUG_LOG.debug("JobServiceImpl.checkAndRemind(): failed while mail sending" + e.toString());
+                    e.printStackTrace();
+                }
             });
         }
     }
--- src/main/java/com/reportum/angular2/springmvc/service/impl/ProjectServiceImpl.java
+++ src/main/java/com/reportum/angular2/springmvc/service/impl/ProjectServiceImpl.java
@@ -6,16 +6,18 @@ import com.reportum.angular2.springmvc.persistence.entities.User;
 import com.reportum.angular2.springmvc.service.IEmailService;
 import com.reportum.angular2.springmvc.service.IProjectService;
 import com.reportum.angular2.springmvc.utils.enums.State;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
-@PropertySource("classpath:application.properties")
 public class ProjectServiceImpl implements IProjectService{
+
+    private final Logger DEBUG_LOG = LoggerFactory.getLogger("DEBUG_LOGGER");
 
     @Autowired
     private IProjectDAO projectDAO;
@@ -43,7 +45,12 @@ public class ProjectServiceImpl implements IProjectService{
     public void saveProject(Project project) {
         projectDAO.saveProject(project);
         if(State.REVIEWED.getValue().equals(project.getState())){
-            emailService.sendSimpleMessage(managerEmail,subject,project.getProjectName() + " report is done.");
+            try {
+                emailService.sendSimpleMessage(managerEmail, subject, project.getProjectName() + " report is done.");
+            } catch (Exception e){
+                DEBUG_LOG.debug("ProjectServiceImpl.saveProject(): failed while mail sending" + e.toString());
+                e.printStackTrace();
+            }
         }
     }
 
